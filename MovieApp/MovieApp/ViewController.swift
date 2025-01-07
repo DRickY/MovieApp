6e6fde01279195fb98f554f8460f39ce52d17df8
Dmytro <dmitry.k792@gmail.com>
2025-01-07 21:17:41 +0200

    build: Initial project setup

---
+++ MovieApp/MovieApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by Dmytro Kozak on 07.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
