5f9fd3cb0f216405025bf7a9decb1e7f1e1c89e6
Dmytro <dmitry.k792@gmail.com>
2025-01-08 10:50:21 +0200

    feat: add Environment, AppConfig, Create Entry directory.

---
+++ MovieApp/MovieApp/Entry/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  MovieApp
+//
+//  Created by Dmytro on 08.01.2025.
+//
+
+import Foundation
+import UIKit
+
+let defaultCornerRadius = CGFloat(12)
+
+let defaultRadius = CGFloat(16)
+
+let consoleLogPattern: String = "<where><what>\n<log>\n"
+
+let kGenrePollingInterval: TimeInterval = 300
+
+let debounce: OperationQueue.SchedulerTimeType.Stride = 1.1
