69121d68dd5ba1429d3d1323b07412a91c5845ee
Dmytro <dmitry.k792@gmail.com>
2025-01-08 11:46:48 +0200

    feat: add Network Monitor

--- MovieApp/MovieApp/Entry/AppDelegate.swift
+++ MovieApp/MovieApp/Entry/AppDelegate.swift
@@ -24,3 +24,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
     }
 }
+
+let log = LoggerHub()
